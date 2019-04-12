b80d75d91100ffde0b189e71afe94c5607cf37ec
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2019-04-12 17:08:05 +0100

    - Updated map for Line L4 showing new connections. - Added package Map::Tube::Madrid::Line::L4 - Added map image for Line L4.

--- lib/Map/Tube/Madrid.pm
+++ lib/Map/Tube/Madrid.pm
@@ -1,6 +1,6 @@
 package Map::Tube::Madrid;
 
-$Map::Tube::Madrid::VERSION   = '0.31';
+$Map::Tube::Madrid::VERSION   = '0.32';
 $Map::Tube::Madrid::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Map::Tube::Madrid - Interface to the Madrid Metro Map.
 
 =head1 VERSION
 
-Version 0.31
+Version 0.32
 
 =cut
 
